6d621379af60dfe93d11fdf21bb4f9d03945daa3
aliya9686 <aliyamohammadi043@gmail.com>
2024-10-29 18:17:05 +0000

    datasummary

---
+++ .devcontainer/01_basics/datasummary.js
@@ -0,0 +1,11 @@
+//Primitive
+//7 types:String,Boolean,BigInt,Symbol,Undefined,Symbol,Number
+//Non Primitives(Reference)
+//Strings,Functions,Objects
+const score=100
+const scoreValue=100.3
+const isLoggedIn=false
+const outsideTemp=null
+let userEmail;
+const id=Symbol('123')
+const anotherid=Symbol('123')
