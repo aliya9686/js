d4d78beca25bb2639c7877928d10aa4b24501329
aliya9686 <aliyamohammadi043@gmail.com>
2024-10-29 15:09:37 +0000

    conversion of data types

---
+++ .devcontainer/01_basics/dataconversions.js
@@ -0,0 +1,25 @@
+let score="aliya"
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber=Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+//"33"=>33
+//"33abc"=>Nan
+//true=>1
+//false=>0
+
+let isloggedin="aliya"
+let booleanisloggedin=Boolean(isloggedin);
+console.log(booleanisloggedin);
+
+//if something=true or nothing empty=false
+//1=>True; 0=>false;
+//""=>false
+//"aliya"=>true
+
+let someNumber=32;
+let stringNumber=String(someNumber);
+console.log(typeof stringNumber);
