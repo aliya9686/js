3d862f57ec1882438edef140f106eb2e06445eb6
aliya9686 <aliyamohammadi043@gmail.com>
2024-10-29 14:48:50 +0000

    variables

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,21 @@
+//Print statement
+console.log("Aliya");
+
+const id=123;
+let name="aliya";
+var city="mysore";
+
+console.log(id);//id is constant cannot be changed
+id=456;
+name="alfiya";//it can be changed
+city="bangalore";//var is not usuualy preferred due to block and function scope
+console.table([name,city]);//to print all the entries once in a table
+alert(3+3);//it is not supported in node js but it supports browser
+
+//Datatypes
+console.log(typeof "aliya");//String
+console.log(typeof 123);//Number
+console.log(typeof true);//Boolean
+console.log(typeof undefined);//Undefined
+console.log(typeof null);//object
+console.log(typeof boolean);//Undefined
